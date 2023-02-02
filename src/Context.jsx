613b132334e3ebcac9b78aee6382b8a5affe7851
kadjialan <alankadji7@gmail.com>
2023-02-02 11:17:26 +0100

    linters modification

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,7 +1,7 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 const QuestionContext = createContext();
 const QuestionProvider = QuestionContext.Provider;
 const QuestionConsumer = QuestionContext.Consumer;
 
-export { QuestionProvider, QuestionConsumer, QuestionContext }
+export { QuestionProvider, QuestionConsumer, QuestionContext };
