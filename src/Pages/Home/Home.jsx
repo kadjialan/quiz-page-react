2f753077cfee504e301731901d0ac3f5250fcbc0
kadjialan <alankadji7@gmail.com>
2024-02-23 14:35:08 +0100

    updating home page design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import QuizPic from '../../Images/quiz.jpg';
 import Button from '../../Components/Button/Button';
 
 function Home() {
   return (
-    <div className="home">
-      <h1>WELCOME TO YOUR TEST QUIZ</h1>
-      <p>Move to the next stage by clicking on the button below</p>
-      <img src={QuizPic} alt="quiz" className="home__picture" />
-      <Button
-        text={
-          <Link to="question/1" className="home__link">
-            Get Started
-          </Link>
-        }
-      />
-    </div>
+    <section className="home">
+      <div className="home_content">
+        <h1>WELCOME TO YOUR TEST QUIZ</h1>
+        <p>Move to the next stage by clicking on the button below</p>
+        <Button
+          text={
+            <Link to="question/1" className="home__link">
+              Get Started
+            </Link>
+          }
+        />
+      </div>
+    </section>
   );
 }
 
